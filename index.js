b4abb7cf952b7d0cc1c065ef741a7ec9038fa8ec
Taketakemagic <tandritikin@gmail.com>
2024-05-30 19:26:55 +0900

    初期

---
+++ index.js
@@ -0,0 +1,13 @@
+function buttonClick() {
+  var befor = document.getElementById('befor').value;
+  var after = convertYoutubeLiveUrl(befor);
+}
+function convertYoutubeLiveUrl(befor) {
+  var livePattern = /^https:\/\/www\.youtube\.com\/live\/([a-zA-Z0-9_-]+)$/;
+  var match = befor.match(livePattern);
+  if (match) {
+    return 'https://www.youtube.com/watch?v=' + match[1];
+  } else {
+    return '入力されたURLは有効なYouTubeライブURLではありません。';
+  }
+}
