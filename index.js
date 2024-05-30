64e55a1d771acec10044e43d8393f2b8f2ba002c
Taketakemagic <tandritikin@gmail.com>
2024-05-30 21:06:53 +0900

    一旦完成版

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 function buttonClick() {
   var befor = document.getElementById('befor').value;
   var after = convertYoutubeLiveUrl(befor);
-  document.getElementById('after').innerText = after;
+  var outputDiv = document.getElementById('after');
+  outputDiv.innerHTML = ''; // 出力をクリア
+
+
+  if (after == '入力されたURLは有効なYouTubeライブURLではありません。') {
+    outputDiv.innerText = after;
+  } else {
+    var link = createLink(after);
+    outputDiv.appendChild(link);
+  }
 }
+
+
 function convertYoutubeLiveUrl(befor) {
-  var livePattern = /^https:\/\/www\.youtube\.com\/live\/([a-zA-Z0-9_-]+)$/;
-  var match = befor.match(livePattern);
-  if (match) {
-    return 'https://www.youtube.com/watch?v=' + match[1];
+  var livePattern = 'https://www.youtube.com/live/';
+  console.log("befor:" + befor);
+  console.log("livePattern:" + livePattern);
+  if (befor.indexOf(livePattern) === 0) {
+    // 前方一致のときの処理
+    return befor.replace('https://www.youtube.com/live/', 'https://www.youtube.com/watch?v=');
   } else {
     return '入力されたURLは有効なYouTubeライブURLではありません。';
   }
+}
+
+function createLink(after) {
+  var anchor = document.createElement('a');
+  anchor.href = after;
+  anchor.innerText = after;
+  anchor.target = '_blank'; // リンクを新しいタブで開く
+  return anchor;
 }
